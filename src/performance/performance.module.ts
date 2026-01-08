be0b35802d9c873e6311349f5a4fcf6a05940dac
greatit-lab <gil2zum@gmail.com>
2026-01-08 19:19:21 +0900

    Create performance.module.ts --0108_19

---
+++ src/performance/performance.module.ts
@@ -0,0 +1,11 @@
+// ITM-Data-API/src/performance/performance.module.ts
+import { Module } from '@nestjs/common';
+import { PerformanceController } from './performance.controller';
+import { PerformanceService } from './performance.service';
+import { PrismaService } from '../prisma.service';
+
+@Module({
+  controllers: [PerformanceController],
+  providers: [PerformanceService, PrismaService],
+})
+export class PerformanceModule {}
