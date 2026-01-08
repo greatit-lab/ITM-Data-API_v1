f1983a7da1586061614bbfbdcdff83859f29eca0
greatit-lab <gil2zum@gmail.com>
2026-01-08 19:07:10 +0900

    Update app.module.ts --0108_19

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
-// itm-data-api/src/app.module.ts
+// ITM-Data-API/src/app.module.ts
 import { Module } from '@nestjs/common';
-import { WaferModule } from './wafer/wafer.module'; // [1] import 추가
+import { PrismaService } from './prisma.service';
+import { WaferModule } from './wafer/wafer.module'; // 기존
+// [추가]
+import { PreAlignModule } from './prealign/prealign.module';
+import { PerformanceModule } from './performance/performance.module';
+import { ErrorModule } from './error/error.module';
+import { LampLifeModule } from './lamplife/lamplife.module';
 
 @Module({
-  imports: [WaferModule], // [2] imports에 등록
-  controllers: [],
-  providers: [],
+  imports: [
+    WaferModule,
+    PreAlignModule,
+    PerformanceModule,
+    ErrorModule,
+    LampLifeModule, // 등록
+  ],
+  providers: [PrismaService],
 })
 export class AppModule {}
