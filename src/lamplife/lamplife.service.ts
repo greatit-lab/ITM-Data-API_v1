6acc625801e4f5681ebb5fce27b6d7ca2d2d8894
greatit-lab <gil2zum@gmail.com>
2026-01-08 19:10:22 +0900

    Update lamplife.service.ts --0108_19

--- src/lamplife/lamplife.service.ts
+++ src/lamplife/lamplife.service.ts
@@ -1,4 +1,4 @@
-// ITM-Data-API/src/lamplife/lamplife.service.ts)
+// ITM-Data-API/src/lamplife/lamplife.service.ts
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma.service';
 
@@ -7,10 +7,20 @@ export class LampLifeService {
   constructor(private prisma: PrismaService) {}
 
   async getLampStatus(site: string, sdwt: string) {
-    // 최신 램프 상태 조회
-    // RefLampLife 테이블 가정
-    return this.prisma.refLampLife.findMany({
-      // where: { site, sdwt } // 필요 시 장비 테이블과 Join 조건 추가
+    return this.prisma.eqpLampLife.findMany({
+      where: {
+        // [수정] site, sdwt 변수를 사용하여 장비 필터링 적용
+        equipment: {
+          // sdwt 값이 있으면 필터링
+          ...(sdwt && { sdwt: sdwt }),
+          // site 값이 있으면 RefSdwt 관계를 통해 필터링
+          ...(site && {
+            sdwtRel: {
+              site: site
+            }
+          })
+        }
+      },
       orderBy: { eqpid: 'asc' }
     });
   }
