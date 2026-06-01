7065dbfd6253b5388300bd7b4fcfba3bf81641c0
greatit-lab <gil2zum@gmail.com>
2026-06-01 09:07:09 +0900

    Add ArchivePrismaService to WaferModule providers --0601_09

--- src/wafer/wafer.module.ts
+++ src/wafer/wafer.module.ts
@@ -3,9 +3,10 @@ import { Module } from '@nestjs/common';
 import { WaferController } from './wafer.controller';
 import { WaferService } from './wafer.service';
 import { PrismaService } from '../prisma.service';
+import { ArchivePrismaService } from '../archive-prisma.service'; // [추가됨] Archive DB 연결 서비스
 
 @Module({
   controllers: [WaferController],
-  providers: [WaferService, PrismaService],
+  providers: [WaferService, PrismaService, ArchivePrismaService], // [추가됨] 의존성 주입
 })
 export class WaferModule {}
